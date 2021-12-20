6cd6c6fffec2b366f5b418aeab7f4d1cda910b79
João Pedro Fernandes <joao.pedro.fernandes.122@gmail.com>
2021-12-20 15:45:34 -0300

    crianda a pagina da ideia

---
+++ infojr-insights/src/components/ideia/IdeiaStyled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Ideia = styled.article`
+  display: flex;
+  flex-direction: column;
+`
---
+++ infojr-insights/src/styles/PagIdeiaStyled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const PagIdeiaStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 120px;
+  padding-top: 84px;
+  color: #fff;
+  background-color: #1a1c17;
+`
+
+export default PagIdeiaStyled
