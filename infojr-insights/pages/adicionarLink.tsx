55f325d467bc7c00f1059cf784dad5eefbead581
João Pedro Fernandes <joao.pedro.fernandes.122@gmail.com>
2021-12-27 15:23:12 -0300

    começando o popup do form

--- infojr-insights/pages/adicionarLink.tsx
+++ infojr-insights/pages/adicionarLink.tsx
@@ -1,10 +1,16 @@
 import { NextPage } from 'next'
+import Image from 'next/image'
 
+import Close from '../public/images/Close.svg'
 import Form from '../src/components/form/Form'
 import AdicionarLinkStyled from '../src/styles/AdicionarLinkStyled'
 
 const AdicionarLink: NextPage = () => (
   <AdicionarLinkStyled>
+    <div className="popup">
+      <Image src={Close} />
+      <p className="popup_text"> Link cadastrado com sucesso</p>
+    </div>
     <h2 className="titulo_pag">Adicionar Link</h2>
     <Form />
   </AdicionarLinkStyled>
